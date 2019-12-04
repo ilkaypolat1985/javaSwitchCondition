0c8f0d32cef439296ac40850397560baf08615d3
ilkaypolat1985 <ilkaypolat1985@gmail.com>
2019-12-04 21:37:19 +0100

    Swtich cases added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        char grade = 'C';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Great! Passed!");
+                break;
+            case 'B':
+                System.out.println("Very Good! Passed!");
+                break;
+            case 'C':
+                System.out.println("Good! Passed!");
+                break;
+            case 'D':
+                System.out.println("Not Bad! Passed!");
+                break;
+            case 'F':
+                System.out.println("Unfortunately! Failed!");
+                break;
+            default:
+                System.out.println("Invalid Grade!");
+        }
     }
 }
